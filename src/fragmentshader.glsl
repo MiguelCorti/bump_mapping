c8718740bcb1b54413588f4fdd8de4794b506585
Miguel Corti <miguelszcorti@gmail.com>
2021-05-27 16:43:00 -0300

    adds some comments and changes location of some calcs on fragment shader

--- src/fragmentshader.glsl
+++ src/fragmentshader.glsl
@@ -20,37 +20,9 @@ in vec4 fragPosLightSpace; // Posicao do fragmento no espaco da luz
 
 out vec3 finalColor;
 
-float isInShadow(vec3 N, vec3 L)
-{
-    vec3 projCoords = fragPosLightSpace.xyz / fragPosLightSpace.w;
-    projCoords = projCoords * 0.5 + 0.5; // Transforma de [-1,1]->[0,1]
-
-    // Se estiver além do far da luz deixa sem sombra
-    if(projCoords.z > 1.0)
-        return 0.0;
-
-    float closestDepth = texture(shadowMap, projCoords.xy).r; // Pega a profundidade do mapa de sombras
-    float currentDepth = float(projCoords.z); // Pega a profundidade atual
-
-    float bias = max(0.05 * (1.0 - dot(N, L)), 0.005); // Calcula um offset para remover Acne
-    float shadow = 0.0;
-    vec2 texelSize = 1.0/textureSize(shadowMap, 0);
-    for(int x=-1; x<=1; x++)
-    {
-        for(int y=-1;y<=1;y++)
-        {
-            float depthPCF = texture(shadowMap, projCoords.xy + vec2(x, y)*texelSize).r;
-            shadow += (currentDepth-bias) > depthPCF ? 1.0 : 0.0;
-        }
-    }
-    shadow /= 9.0;
-
-
-    return shadow;
-}
-
 void main()
 {
+    // CALCULO DA COR
     vec3 textureRGB = texture(sampler, fragUV).rgb;
     vec3 ambient = material.ambient * textureRGB;
 
@@ -66,6 +38,31 @@ void main()
     float iSpec = pow(max(dot(N,H),0.0), material.shininess);
     vec3 specular = iSpec * material.specular * textureRGB;
 
-    float shadow = isInShadow(N,L);
+    // CALCULO DAS SOMBRAS
+    vec3 projCoords = fragPosLightSpace.xyz / fragPosLightSpace.w;
+    projCoords = projCoords * 0.5 + 0.5; // Transforma de [-1,1]->[0,1]
+    float shadow = 0.0;
+    // Se estiver além do far da luz deixa sem sombra
+    if(projCoords.z <= 1.0) {
+        float closestDepth = texture(shadowMap, projCoords.xy).r; // Pega a profundidade do mapa de sombras
+        float currentDepth = float(projCoords.z); // Pega a profundidade atual
+
+        float bias = max(0.05 * (1.0 - dot(N, L)), 0.005); // Calcula um offset para remover Acne
+
+        // CALCULO DO PCF
+//        int num_filter = 3;
+        vec2 texelSize = 1.0/textureSize(shadowMap, 0);
+        for(int x=-1; x<=1; x++)
+        {
+            for(int y=-1;y<=1;y++)
+            {
+                float depthPCF = texture(shadowMap, projCoords.xy + vec2(x, y)*texelSize).r;
+                shadow += (currentDepth-bias) > depthPCF ? 1.0 : 0.0;
+            }
+        }
+        shadow /= 9.0;
+    }
+
+    // MONTANDO A COR FINAL
     finalColor = (ambient + (1.0-shadow) * (diffuse + specular));
 }
